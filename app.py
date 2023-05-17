f3957c2dd79753d450172084c0e8a82c3838ff31
PavaniNamburi2412 <133837966+PavaniNamburi2412@users.noreply.github.com>
2023-05-17 00:08:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,110 +1,158 @@
+import cv2
+import os
+import numpy as np
+import json
+import argparse
+
 import streamlit as st
-import yfinance as yf
-import pandas as pd
-from vega_datasets import data
-import plotly.graph_objects as go
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.mention import mention
-from datetime import date
 
 
-# Set page title
-# تحديد الكلمات المفتاحية المستهدفة لموقعك
-keywords = ["Rasheed Al-qadhi", "Rasheed al qadhi", "project to obtain the stock market data"]
-
-# إنشاء عنوان صفحة موقعك مع تضمين الكلمات المفتاحية
-st.title("Hello, I'm RASHEED AL-QADHI. This is a project to obtain the stock market data .")
-
-# إنشاء محتوى الصفحة مع تضمين الكلمات المفتاحية
-st.write("Streamlit is an open-source app framework for Machine Learning and Data Science teams. Create beautiful web apps in minutes. The software libraries used in the project are streamlit , yfinance, pandas, vega_datasets, plotly, streamlit_extras")
-
-
-# colored_header(
-#     label="Hi , i'm RASHEED AL-QADHI ",
-#     description="",
-#     color_name="orange-70",
-# )
-
-# Set up social media icons
-with st.container():
-    col1, col2, col3 = st.columns(3)
-    col1.markdown("[![Twitter](https://img.shields.io/twitter/follow/shadowYEM?style=social)](https://twitter.com/shadowYEM)")
-    col2.markdown("[![GitHub](https://img.shields.io/github/followers/shadowYEM?style=social)](https://github.com/shadowYEM)")
-    col3.markdown("[![Telegram](https://img.shields.io/badge/-Telegram-white?color=white&logo=telegram&logoColor=blue)](https://t.me/ShadowYE)")
-    # col4.markdown("[![Kaggle](https://img.shields.io/badge/Kaggle-035a7d?style=for-the-badge&color=white&logo=kaggle&logoColor=blue)](https://kaggle.com/shadowYE)")
+@st.cache
+def get_arguments():
+    """Return the values of CLI params"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_folder", default="images")
+    parser.add_argument("--image_width", default=400, type=int)
+    args = parser.parse_args()
+    return getattr(args, "image_folder"), getattr(args, "image_width")
 
 
-colored_header(
-    label="A project containing 20 stock exchange shares",
-    description="",
-    color_name="orange-70",
-)
+@st.cache
+def get_images_list(path_to_folder: str) -> list:
+    """Return the list of images from folder
+    Args:
+        path_to_folder (str): absolute or relative path to the folder with images
+    """
+    image_names_list = [
+        x for x in os.listdir(path_to_folder) if x[-3:] in ["jpg", "peg", "png"]
+    ]
+    return image_names_list
 
 
-
-st.sidebar.title('Stock Data :part_alternation_mark:')
-
-# Set page title
-
-# Define the user inputs
-tickers = st.sidebar.selectbox('Select Tickers', ['AAPL','MSFT', 'AMZN', 'GOOGL', 'META', 'TSLA', 'BRK.A', 'BRK.B', 
-                                                  'NVDA', 'JPM', 'JNJ', 'V', 'PG', 'DIS','INTC', 'PFE', 'KO', 'XOM', 'MCD', 'IBM' ], index=0,  format_func=lambda x: x.upper())
-today = date.today()
-
-start_date = st.sidebar.date_input('Start date', value=pd.to_datetime('2022-03-22'))
-end_date = st.sidebar.date_input('End date', value=today)
-
-# Fetch stock data from Yahoo
-data = yf.download(tickers, start=start_date, end=end_date)
-
-# Display stock data
-st.header(f'Stock data for :red[{tickers}]')
-st.dataframe(data, width=1000)
+@st.cache
+def load_image(image_name: str, path_to_folder: str, bgr2rgb: bool = True):
+    """Load the image
+    Args:
+        image_name (str): name of the image
+        path_to_folder (str): path to the folder with image
+        bgr2rgb (bool): converts BGR image to RGB if True
+    """
+    path_to_image = os.path.join(path_to_folder, image_name)
+    image = cv2.imread(path_to_image)
+    if bgr2rgb:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
 
 
-st.title('Data visualization :zap:')
-
-# Set up plotly figure
-fig = go.Figure()
-
-
-
-# Add candlestick trace for stock prices
-fig.add_trace(go.Candlestick(x=data.index,
-                             open=data['Open'],
-                             high=data['High'],
-                             low=data['Low'],
-                             close=data['Close'],
-                             name="Stock Prices"))
-
-# Add moving average trace for 50-day period
-fig.add_trace(go.Scatter(x=data.index,
-                         y=data['Close'],
-                         mode='lines',
-                         name='Moving Average (50 days)'))
-
-# Add plotly layout
-fig.update_layout(title=f"{tickers} Stock Prices",
-                  xaxis_title="Date",
-                  yaxis_title="Price (USD)")
-
-# Render plotly figure using streamlit
-st.plotly_chart(fig)
-
-# Add download button to download data
+def upload_image(bgr2rgb: bool = True):
+    """Uoload the image
+    Args:
+        bgr2rgb (bool): converts BGR image to RGB if True
+    """
+    file = st.sidebar.file_uploader(
+        "Upload your image (jpg, jpeg, or png)", ["jpg", "jpeg", "png"]
+    )
+    image = cv2.imdecode(np.fromstring(file.read(), np.uint8), 1)
+    if bgr2rgb:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
 
 
-@st.cache_resource
-def convert_df(df):
-    st.title(f'Data :red[{tickers}] Download ')
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
+@st.cache
+def load_augmentations_config(
+    placeholder_params: dict, path_to_config: str = "configs/augmentations.json"
+) -> dict:
+    """Load the json config with params of all transforms
+    Args:
+        placeholder_params (dict): dict with values of placeholders
+        path_to_config (str): path to the json config file
+    """
+    with open(path_to_config, "r") as config_file:
+        augmentations = json.load(config_file)
+    for name, params in augmentations.items():
+        params = [fill_placeholders(param, placeholder_params) for param in params]
+    return augmentations
 
-csv = convert_df(data)
 
-st.download_button(
-    label="Download data as CSV",
-    data=csv,
-    file_name=f'{tickers}.csv',
-    mime='text/csv',
-)
+def fill_placeholders(params: dict, placeholder_params: dict) -> dict:
+    """Fill the placeholder values in the config file
+    Args:
+        params (dict): original params dict with placeholders
+        placeholder_params (dict): dict with values of placeholders
+    """
+    # TODO: refactor
+    if "placeholder" in params:
+        placeholder_dict = params["placeholder"]
+        for k, v in placeholder_dict.items():
+            if isinstance(v, list):
+                params[k] = []
+                for element in v:
+                    if element in placeholder_params:
+                        params[k].append(placeholder_params[element])
+                    else:
+                        params[k].append(element)
+            else:
+                if v in placeholder_params:
+                    params[k] = placeholder_params[v]
+                else:
+                    params[k] = v
+        params.pop("placeholder")
+    return params
+
+
+def get_params_string(param_values: dict) -> str:
+    """Generate the string from the dict with parameters
+    Args:
+        param_values (dict): dict of "param_name" -> "param_value"
+    """
+    params_string = ", ".join(
+        [k + "=" + str(param_values[k]) for k in param_values.keys()]
+    )
+    return params_string
+
+
+def get_placeholder_params(image):
+    return {
+        "image_width": image.shape[1],
+        "image_height": image.shape[0],
+        "image_half_width": int(image.shape[1] / 2),
+        "image_half_height": int(image.shape[0] / 2),
+    }
+
+
+def select_transformations(augmentations: dict, interface_type: str) -> list:
+    # in the Simple mode you can choose only one transform
+    if interface_type == "Simple":
+        transform_names = [
+            st.sidebar.selectbox(
+                "Select a transformation:", sorted(list(augmentations.keys()))
+            )
+        ]
+    # in the professional mode you can choose several transforms
+    elif interface_type == "Professional":
+        transform_names = [
+            st.sidebar.selectbox(
+                "Select transformation №1:", sorted(list(augmentations.keys()))
+            )
+        ]
+        while transform_names[-1] != "None":
+            transform_names.append(
+                st.sidebar.selectbox(
+                    f"Select transformation №{len(transform_names) + 1}:",
+                    ["None"] + sorted(list(augmentations.keys())),
+                )
+            )
+        transform_names = transform_names[:-1]
+    return transform_names
+
+
+def show_random_params(data: dict, interface_type: str = "Professional"):
+    """Shows random params used for transformation (from A.ReplayCompose)"""
+    if interface_type == "Professional":
+        st.subheader("Random params used")
+        random_values = {}
+        for applied_params in data["replay"]["transforms"]:
+            random_values[
+                applied_params["__class_fullname__"].split(".")[-1]
+            ] = applied_params["params"]
+        st.write(random_values)
